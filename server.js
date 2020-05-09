56edd61b6c0582a9e7d081a2b74de6b17b2a5d86
gototakuma <gota.main@i.softbank.jp>
2020-05-09 13:42:31 +0900

    本番環境設定

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
+
 app.use(history());
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
